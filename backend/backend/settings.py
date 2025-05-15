5112e3b5fc87e0c8c5917c5b72124b2c3b9ad2e8
ChemzRegaton <160089008+ChemzRegaton@users.noreply.github.com>
2025-05-16 00:45:46 +0800

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,6 +21,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
